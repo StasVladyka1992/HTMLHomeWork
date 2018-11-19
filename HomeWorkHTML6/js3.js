7fda76c143504f7b9716d4c037f70095d51e5a25
StasVladyka1992 <stavladyk@gmail.com>
2018-11-19 15:21:33 +0300

    added color changing to the button 'Send review' HW6, task3

--- HomeWorkHTML6/js3.js
+++ HomeWorkHTML6/js3.js
@@ -1,10 +1,18 @@
+
+// Only 2 fields are checking: email and name. 
+// Я могу сделать проверку с помощью регулярных выражений, 
+// изменив тип поля с "email" на "text", но не вижу в этом смысла, 
+// т.к. html имеет функцию автоматической проверки правильности 
+// введенной почты
 setInterval(function submitButton(){
     var form = document.getElementById("form");
     var subButton = document.getElementById("submitButton");
     if (form.name.value == "" || form.email.value==""){
         subButton.disabled = true;
+        subButton.style.cssText = "background-color: red;";
     }
     else {
-        subButton.disabled=false;  
+        subButton.disabled=false; 
+        subButton.style.cssText = "background-color: green;";
     }
 }, 10);
